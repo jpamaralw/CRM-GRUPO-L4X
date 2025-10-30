be6966aead0c9ac5b5f7855739805350525c89f3
jpamaralw <joao.chagas@sempreceub.com>
2025-10-30 12:56:10 -0300

    FIX FINAL: Removido binaryTargets e ajuste de DB

--- src/app/server/actions.js
+++ src/app/server/actions.js
@@ -2,56 +2,64 @@
  * ! The server actions below are used to fetch the static data from the fake-db. If you're using an ORM
  * ! (Object-Relational Mapping) or a database, you can swap the code below with your own database queries.
  */
-'use server'
+"use server";
 
 // Data Imports
-import { db as eCommerceData } from '@/fake-db/apps/ecommerce'
-import { db as academyData } from '@/fake-db/apps/academy'
-import { db as vehicleData } from '@/fake-db/apps/logistics'
-import { db as invoiceData } from '@/fake-db/apps/invoice'
-import { db as userData } from '@/fake-db/apps/userList'
-import { db as permissionData } from '@/fake-db/apps/permissions'
-import { db as profileData } from '@/fake-db/pages/userProfile'
-import { db as faqData } from '@/fake-db/pages/faq'
-import { db as pricingData } from '@/fake-db/pages/pricing'
-import { db as statisticsData } from '@/fake-db/pages/widgetExamples'
+import { db as eCommerceData } from "@/fake-db/apps/ecommerce";
+import { db as academyData } from "@/fake-db/apps/academy";
+import { db as vehicleData } from "@/fake-db/apps/logistics";
+import { db as invoiceData } from "@/fake-db/apps/invoice";
+import { db as userData } from "@/fake-db/apps/userList";
+import { db as permissionData } from "@/fake-db/apps/permissions";
+import { db as profileData } from "@/fake-db/pages/userProfile";
+import { db as faqData } from "@/fake-db/pages/faq";
+import { db as pricingData } from "@/fake-db/pages/pricing";
+import { db as statisticsData } from "@/fake-db/pages/widgetExamples";
+import { neon } from "@neondatabase/serverless";
 
 export const getEcommerceData = async () => {
-  return eCommerceData
-}
+  return eCommerceData;
+};
 
 export const getAcademyData = async () => {
-  return academyData
-}
+  return academyData;
+};
 
 export const getLogisticsData = async () => {
-  return vehicleData
-}
+  return vehicleData;
+};
 
 export const getInvoiceData = async () => {
-  return invoiceData
-}
+  return invoiceData;
+};
 
 export const getUserData = async () => {
-  return userData
-}
+  return userData;
+};
 
 export const getPermissionsData = async () => {
-  return permissionData
-}
+  return permissionData;
+};
 
 export const getProfileData = async () => {
-  return profileData
-}
+  return profileData;
+};
 
 export const getFaqData = async () => {
-  return faqData
-}
+  return faqData;
+};
 
 export const getPricingData = async () => {
-  return pricingData
-}
+  return pricingData;
+};
 
 export const getStatisticsData = async () => {
-  return statisticsData
+  return statisticsData;
+};
+
+export async function getData() {
+  const sql = neon(process.env.DATABASE_URL);
+  // Replace the query below with your real query
+  const data = await sql`SELECT now() as now;`;
+  return data;
 }
